8f2ca73e64c0078b7d1776d7b098c066fa5e046b
josh <6jkruege@informatik.uni-hamburg.de>
2021-03-11 10:49:43 +0100

    added some doc

--- src/jokrey/utilities/simple/data_structure/queue/Queue.java
+++ src/jokrey/utilities/simple/data_structure/queue/Queue.java
@@ -4,16 +4,21 @@ package jokrey.utilities.simple.data_structure.queue;
  * @author jokrey
  */
 public interface Queue<E> {
+    /**Add given element to queue, must not be null*/
     void enqueue(E e);
+    /**Return and Remove next elem in queue or null*/
     E dequeue();
+    /**Return next elem in queue or null*/
     E peek();
+    /**Calculate the size, might be done by iteration (can be costly)*/
     int size();
+    /**Clears the contents*/
     void clear();
 
 
     //debug or test functionality:
-    void println();
-    void enqueue(E e, long sleep_at_some_point);
-    E dequeue(long sleep_at_some_point);
-    E peek(long sleep_at_some_point);
+    default void println() {throw new UnsupportedOperationException();}
+    default void enqueue(E e, long sleep_at_some_point) {throw new UnsupportedOperationException();}
+    default E dequeue(long sleep_at_some_point) {throw new UnsupportedOperationException();}
+    default E peek(long sleep_at_some_point) {throw new UnsupportedOperationException();}
 }
