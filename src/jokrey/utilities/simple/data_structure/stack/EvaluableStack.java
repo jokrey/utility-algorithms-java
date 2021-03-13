08483fcf2d281ade02a02dcf5b63d09a98f57c82
6jkruege <6jkruege@informatik.uni-hamburg.de>
2021-03-13 10:57:57 +0100

    removed defaults

--- src/jokrey/utilities/simple/data_structure/stack/EvaluableStack.java
+++ src/jokrey/utilities/simple/data_structure/stack/EvaluableStack.java
@@ -2,8 +2,8 @@ package jokrey.utilities.simple.data_structure.stack;
 
 public interface EvaluableStack<E> extends Stack<E> {
     //debug or test functionality:
-    default void println() {throw new UnsupportedOperationException();}
-    default void push(E e, long sleep_at_some_point) {throw new UnsupportedOperationException();}
-    default E pop(long sleep_at_some_point) {throw new UnsupportedOperationException();}
-    default E peek(long sleep_at_some_point) {throw new UnsupportedOperationException();}
+    void println();
+    void push(E e, long sleep_at_some_point);
+    E pop(long sleep_at_some_point);
+    E peek(long sleep_at_some_point);
 }
