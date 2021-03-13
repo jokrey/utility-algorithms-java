5cf7bb89cc62b9c74401b6c29be9bc3e77f7e52c
6jkruege <6jkruege@informatik.uni-hamburg.de>
2021-03-13 10:58:43 +0100

    removed comment

--- src/jokrey/utilities/simple/data_structure/stack/EvaluableStack.java
+++ src/jokrey/utilities/simple/data_structure/stack/EvaluableStack.java
@@ -1,7 +1,6 @@
 package jokrey.utilities.simple.data_structure.stack;
 
 public interface EvaluableStack<E> extends Stack<E> {
-    //debug or test functionality:
     void println();
     void push(E e, long sleep_at_some_point);
     E pop(long sleep_at_some_point);
