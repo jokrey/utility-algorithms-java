028882893f86e3fb25c15d030429ce82397aa570
6jkruege <6jkruege@informatik.uni-hamburg.de>
2021-03-14 12:50:53 +0100

    added minor utilities

---
+++ src/jokrey/utilities/misc/TestUtilities.java
@@ -0,0 +1,39 @@
+package jokrey.utilities.misc;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.Assert.fail;
+
+public class TestUtilities {
+    public static void assertIteratorEquality(Iterator<?>... iterators) {
+        if(iterators.length == 0) return;
+        for(int counter=0;;counter++) {
+            boolean hasNext = getOrFail(iterators.length, "at "+counter, i -> iterators[i].hasNext());
+            if(!hasNext) break;
+            Object next = getOrFail(iterators.length, "at "+counter, i -> iterators[i].next());
+            System.out.println("next("+counter+") = " + next);
+        }
+    }
+
+    public static<E, R> E getOrFail(int max, String additionalFailString, Function<Integer, E> supply) {
+        List<E> supplied = new ArrayList<>();
+        for(int i=0;i<max;i++)
+            supplied.add(supply.apply(i));
+
+        E expectation = supplied.get(0);
+        for(E s:supplied) {
+            if(!expectation.equals(s)) {
+                StringBuilder failStringBuilder = new StringBuilder().append(additionalFailString).append(" - ");
+                for (int i = 0; i < supplied.size(); i++) {
+                    E sb = supplied.get(i);
+                    failStringBuilder.append(i).append("(").append(sb).append("), ");
+                }
+                fail(failStringBuilder.delete(failStringBuilder.length()-2, failStringBuilder.length()).toString());
+            }
+        }
+        return expectation;
+    }
+}
---
+++ src/jokrey/utilities/simple/data_structure/iterators/IteratorUtilities.java
@@ -0,0 +1,30 @@
+package jokrey.utilities.simple.data_structure.iterators;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
+public class IteratorUtilities {
+    public static <E, R> Iterator<R> map(Iterator<E> iterator, Function<E, R> conv) {
+        return new Iterator<R>() {
+            @Override public boolean hasNext() {
+                return iterator.hasNext();
+            }
+            @Override public R next() {
+                return conv.apply(iterator.next());
+            }
+        };
+    }
+
+    public static <E>Iterator<E> reverseIterator(List<E> list) {
+        return new Iterator<E>() {
+            int i = list.size() - 1;
+            @Override public boolean hasNext() {
+                return i >= 0;
+            }
+            @Override public E next() {
+                return list.get(i--);
+            }
+        };
+    }
+}
